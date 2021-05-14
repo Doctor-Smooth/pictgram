6a252b16801de11d587dd1eda83c4e70dbea0a56
EC2 Default User <ec2-user@ip-172-31-27-84.ap-northeast-1.compute.internal>
2021-05-14 12:56:37 +0000

    modified

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20210511113839) do
+ActiveRecord::Schema.define(version: 20210514012211) do
+
+  create_table "topics", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.integer  "user_id"
+    t.string   "description"
+    t.string   "image"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
 
   create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "name"
